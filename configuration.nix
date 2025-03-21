6b8e36033982119519aa0aaaf41009f44eedc4d4
emag <lanabe.lanabe@gmail.com>
2025-03-22 02:24:43 +0900

    Add nerdfonts

--- configuration.nix
+++ configuration.nix
@@ -169,13 +169,14 @@
       noto-fonts-cjk-serif
       noto-fonts-cjk-sans
       noto-fonts-emoji
-    ];
+    ] ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+
     fontDir.enable = true;
     fontconfig = {
       defaultFonts = {
         serif = ["Noto Serif CJK JP" "Noto Color Emoji"];
         sansSerif = ["Noto Sans CJK JP" "Noto Color Emoji"];
-        monospace = ["Noto Color Emoji"];
+        monospace = ["JetBrainsMono Nerd Font" "Noto Color Emoji"];
         emoji = ["Noto Color Emoji"];
       };
     };
